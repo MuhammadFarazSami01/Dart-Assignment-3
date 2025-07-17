5f887eaa6de35553f8c8c7bd229b7c9f8f04f32e
MuhammadFarazSami01 <farazarain46@gmail.com>
2025-07-18 00:05:51 +0500

    Assignment Complete

---
+++ assignment3.dart
@@ -0,0 +1,116 @@
+void main() {
+  // //Q1
+
+  var length = 5;
+  var breadth = 7;
+
+  if (length == breadth) {
+    print("It's Square");
+  } else {
+    print("It's Rectangle");
+  }
+
+  // //Q2
+
+  // var age = 41;
+
+  // if (age >= 18 && age < 40) {
+  //   print("Young");
+  // } else if (age > 40) {
+  //   print("Old");
+  // } else {
+  //   print("Minor");
+  // }
+
+  // //Q3
+
+  // var classHeld = 16;
+  // var classAttend = 10;
+
+  // var percentage = classAttend / classHeld * 100;
+  // // print(percentage);
+
+  // if (percentage >= 75) {
+  //   print(
+  //     "You are eligible \nYour Attendance is ${percentage.toStringAsFixed(2)}",
+  //   );
+  // } else {
+  //   print("You are not eligible \nYour Attendance is below 75%");
+  // }
+
+  // //Q4
+
+  // var year = 2023;
+
+  // if (year % 4 == 0) {
+  //   print("${year} is a Leap Year");
+  // } else {
+  //   print("${year} is not a Leap Year");
+  // }
+
+  // //Q5
+
+  // var temperature = 20;
+
+  // if (temperature <= 0) {
+  //   print("Freezing weather");
+  // } else if (temperature > 0 && temperature < 10) {
+  //   print("Very Cold weather");
+  // } else if (temperature >= 10 && temperature < 20) {
+  //   print("Cold weather");
+  // } else if (temperature >= 20 && temperature < 30) {
+  //   print("Normal in Temp");
+  // } else if (temperature >= 30 && temperature < 40) {
+  //   print("Its Hot");
+  // } else if (temperature >= 40) {
+  //   print("Its Very Hot");
+  // } else {
+  //   print("Invalid Value");
+  // }
+
+  // //Q6
+
+  // var marks = 59;
+
+  // if (marks >= 90) {
+  //   print("Grade A");
+  // } else if (marks >= 80 && marks < 90) {
+  //   print("Grade B");
+  // } else if (marks >= 70 && marks < 80) {
+  //   print("Grade C");
+  // } else if (marks >= 60 && marks < 70) {
+  //   print("Grade D");
+  // } else if (marks < 60) {
+  //   print("Grade F");
+  // } else {
+  //   print("Invalid Value");
+  // }
+
+  // //Q7
+
+  // var anyNum = 24;
+
+  // if (anyNum % 2 == 0) {
+  //   print("${anyNum} Even Number");
+  // } else {
+  //   print("${anyNum} Odd Number");
+  // }
+
+  // //Q8
+
+  // var num1 = 20;
+
+  // if (num1 % 3 == 0 && num1 % 5 == 0) {
+  //   print("${num1} is a multiple of 3 and 5");
+  // } else if (num1 % 3 == 0) {
+  //   print("${num1} is a multiple of 3");
+  // } else if (num1 % 5 == 0) {
+  //   print("${num1} is a multiple of 5");
+  // }
+
+  // // SELF TASK USER INPUT
+
+  // print("Your Name");
+  // var name = stdin.readLineSync();
+  // print(name);
+}
